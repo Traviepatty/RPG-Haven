ddc845ea621e53116627a1b01314ebbf703fc6ef
Vbalder7 <toritoab01@gmail.com>
2022-01-17 20:07:52 -0600

    added test commands

--- main.py
+++ main.py
@@ -1,8 +1,24 @@
 import lightbulb
-from secrets import token1
+from secrets import token1, ID1
 
-bot = lightbulb.BotApp(token = token1)
+bot = lightbulb.BotApp(
+    token = token1,
+    default_enabled_guilds=(ID1),
+    prefix = '+',
+    help_class = None
+)
 
+HELP_MESSAGE = """
+Commands Available:
+`Ping` - says Pong!
+`help` - Gets list of Commands.
+"""
 
+@bot.command
+@lightbulb.command('ping', 'Says pong!')
+@lightbulb.implements(lightbulb.PrefixCommand)
+async def ping(ctx):
+    await ctx.respond('Pong!')
+    
 
 bot.run()

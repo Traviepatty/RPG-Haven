63d2d97c78c016a04ae8f3cd2807a57dca27fcfd
Victor A Balderas <64332955+Vbalder7@users.noreply.github.com>
2022-01-20 20:12:52 -0600

    made bot runnable -  needs extensions

--- launcher.py
+++ launcher.py
@@ -1,7 +1,9 @@
+import os
+
 from rpgBot import __version__
 from rpgBot.bot import Bot
 
-import os
+
 
 if os.name != "nt":
     import uvloop
@@ -10,3 +12,4 @@ if os.name != "nt":
 if __name__ == "__main__":
     bot = Bot()
     bot.run()
+    
--- rpgBot/bot/bot.py
+++ rpgBot/bot/bot.py
@@ -6,7 +6,9 @@ from pathlib import Path
 import hikari
 import lightbulb
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
-from pytz import timezone, utc 
+from pytz import timezone, utc
+from rpgBot import __version__
+
 
 class Bot(lightbulb.BotApp):
     def __init__(self) -> None:
@@ -14,20 +16,43 @@ class Bot(lightbulb.BotApp):
         self.scheduler = AsyncIOScheduler()
         self.scheduler.configure(timezone = utc)
         
+        with open("./secrets/token", mode = "r", encoding = "utf-8") as f:
+            token = f.read().strip()
+        
         super().__init__(
-            prefix="-",
-            case_insensitive_prefix_commands=True,
-            
+            prefix = "-",
+            case_insensitive_prefix_commands = True,
+            token = token,
+            intents = hikari.Intents.ALL,
         )
         
-    def setup(self) -> None:
+            
+    def run(self) -> None: # type: ignore
+        self.event_manager.subscribe(hikari.StartingEvent, self.on_starting)
+        self.event_manager.subscribe(hikari.StartedEvent, self.on_started)
+        self.event_manager.subscribe(hikari.StoppingEvent, self.on_stopping)
+        self.event_manager.subscribe(hikari.MessageCreateEvent, self.on_message_create)
+        
+        super().run(
+            activity=hikari.Activity(
+                name=f"-help | Version{__version__}",
+                type=hikari.ActivityType.WATCHING
+            )
+        )
+        
+    async def on_starting(self, event: hikari.StartingEvent) -> None:
         for ext in self.extensions:
             self.load_extensions(f"rpgBot.bot.extensions.{ext}")
             logging.info(f"{ext} extension loaded")
-            
-    def run(self) -> None:
-        self.setup()
-        super().run(
-            
-        )
+    
+    async def on_started(self, event: hikari.StartedEvent) -> None:
+        self.scheduler.start()
+        logging.info("BOT READY!")
         
+    async def on_stopping(self, event: hikari.StoppingEvent) -> None:
+        self.scheduler.shutdown()
+        
+    
+    async def on_message_create(self, event: hikari.MessageCreateEvent) -> None:
+        if isinstance(event.message.channel_id, hikari.DMChannel):
+            return        
